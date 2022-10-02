953dbad3be5cd00c36823a861f377205fde3b769
Cleoclint <joshuasaguit@gmail.com>
2022-10-02 12:50:26 +0800

    Chore: My Account Changers

--- lib/views/home/my-account.dart
+++ lib/views/home/my-account.dart
@@ -1,12 +1,20 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:hain/utils/info-card.dart';
 
-class  MyAccount extends StatelessWidget {
+class MyAccount extends StatelessWidget {
   const MyAccount({super.key});
+
+  static const name = "User's Name";
+  static const allergy = "Allergy Type";
 
   @override
   Widget build(BuildContext context) {
+    var circleAvatar = CircleAvatar(
+      backgroundImage: AssetImage('assets/profile_icon.png'),
+      radius: 50,
+    );
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.teal.shade600,
@@ -15,15 +23,26 @@ class  MyAccount extends StatelessWidget {
           child: Text('MY ACCOUNT'),
         ),
       ),
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Container(
-              child: Image.asset(
-                'assets/profile_icon.png',
-                scale: 2.1,
-              ),
+      body: SafeArea(
+        child: Column(children: <Widget>[
+          circleAvatar,
+          Text(
+            "Sample User",
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ],
+          SizedBox(
+            height: 20,
+            width: 200,
+            child: Divider(color: Colors.teal),
+          ),
+          InfoCard(text: name, icon: Icons.person, onPressed: () async {}),
+          InfoCard(text: allergy, icon: Icons.person, onPressed: () async {}),
+        ]),
       ),
+    );
+  }
+}
